8bfb9c4462a5e2bf1880c110f26759236868b236
shashankvikashji <shashankdubey244@gmail.com>
2025-04-16 16:30:03 +0530

    Classes and object

---
+++ Class.js
@@ -0,0 +1,66 @@
+// reduse of code is inheritance 
+
+// class ClassName{
+//      constructor(prop1 , prop2){
+//         this.prop1 = prop1;
+//         this.prop2 = prop2;
+//      }
+// }
+
+// let obj = new ClassName("arg1" , "arg2");
+// console.log(obj.prop1);
+// console.log(obj.prop2);
+
+// this keyword refer to obj refers to the obj it belong to 
+
+
+
+
+// class Dog{
+//     constructor(dogname , color , weight , breed){
+//         this.dogname = dogname;
+//         this.color = color;
+//         this.weight = weight;
+//         this.breed = breed;
+//     }
+// }
+ 
+// let dog = new Dog("tomy" , "black" ," 5.5" , " german");
+// console.log(dog.dogname , dog.breed, dog.color ,dog.weight);
+
+
+//------------------------------------  ***CLASSES***  ---------------------------------------------------//
+
+// it is imp to class name is capital 
+
+// class Person{
+//     constructor(firstname , lastname){
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+// }
+
+// let p = new Person("Shivam" , "Verma");
+// console.log(p.firstname , p.lastname);
+
+
+
+
+///----------- *** Method :- function on a class is called a method 
+
+
+// class Person{
+//     constructor(firstname , lastname){
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+
+
+// greet(){
+//     console.log("Hii there hlo ",this.firstname);
+    
+// }
+
+// }
+// let p = new Person("shivam" , "Verma");
+// p.greet();
