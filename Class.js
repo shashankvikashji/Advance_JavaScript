3da7a1a12fbac1cc437e26830f81ec04dd228cc7
shashankvikashji <shashankdubey244@gmail.com>
2025-04-23 15:25:24 +0530

    Prototype

--- Class.js
+++ Class.js
@@ -120,33 +120,97 @@
 
 // Getter and setter 
 
-class Person{
-    #firstname;
-    #lastname;
+// class Person{
+//     #firstname;
+//     #lastname;
     
+//     constructor(firstname , lastname){
+//         this.#firstname = firstname;
+//         this.lastname = lastname;
+//     }
+//     get firstname(){
+//         return this.#firstname;
+//     }
+//     set firstname(firstname){
+//          this.#firstname = firstname;
+//     }
+//     get lastname(){
+//         return this.#lastname;
+//     }
+//     set lastname(lastname){
+//         this.#lastname = lastname;
+//     }
+// }
+// let p = new Person("Shivam" , "Dubey");
+// console.log(p.firstname);
+// p.firstname = "hello";
+// console.log(p.firstname);
+// console.log(p);
+
+
+//--- Inheritance -- 
+
+
+// class vechicle{
+//     constructor(color , currentSpeed , maxspeed){
+//         this.color = color;
+//         this.currentSpeed = currentSpeed;
+//         this.maxspeed = maxspeed;
+//     }
+//     move(){
+//         console.log("Moving at " , this.currentSpeed);
+        
+//     }
+//     accelerate(amount){
+//         this.currentSpeed += amount;
+//     }
+// }
+// class Motorcycle extends vechicle{
+//     constructor(color,currentSpeed,maxspeed,fuel1){
+//         super(color,currentSpeed,maxspeed);
+//         this.fuel1 = fuel1;
+//     }
+//    doWheelier(){
+//     console.log("Driving on one wheel ");
+    
+//    }
+
+// }
+// let motor = new Motorcycle("Black" , 0 , 250 , "gasoline");
+// console.log(motor.color);
+// motor.accelerate(30);
+// motor.move();
+
+
+
+
+// ------ ** Prototypes :- In JavaScript, the prototype property allows you to add new properties and methods to object constructors. 
+// This is a key feature of JavaScript's prototype-based inheritance system.
+
+
+class Person{
     constructor(firstname , lastname){
-        this.#firstname = firstname;
+        this.firstname = firstname;        
         this.lastname = lastname;
     }
-    get firstname(){
-        return this.#firstname;
-    }
-    set firstname(firstname){
-         this.#firstname = firstname;
-    }
-    get lastname(){
-        return this.#lastname;
-    }
-    set lastname(lastname){
-        this.#lastname = lastname;
-    }
-}
-let p = new Person("Shivam" , "Dubey");
-console.log(p.firstname);
-p.firstname = "hello";
-console.log(p.firstname);
-console.log(p);
 
+
+greet(){
+    console.log("Hii there hlo ",this.firstname);
+    
+}
+}
+
+Person.prototype.introduce = function (){
+     console.log("Hi i am " , this.firstname);
+     
+};
+
+Person.prototype.favcolor = "green";
+
+let p = new Person("Shivam" , "verma");
+console.log(p.favcolor);
+p.introduce();
 
 
 
